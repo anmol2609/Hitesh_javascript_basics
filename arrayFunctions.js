231e1b02952f4510c9fa7e1f5a29b01a3a873f41
anmol2609 <anmoldwivedi145@gmail.com>
2024-07-20 22:14:40 +0530

    added some more array methods

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -40,5 +40,26 @@ const myN2 = myArray.splice(1, 3)
 console.log(myN2) // [ 1, 2, 3 ]
 console.log("C ", myArray); // C  [ 0, 4, 5 ] => splice manipulate original array but slice can't
 
+console.log('++++++++++++++++++++ Merge or concat arrays ++++++++++++++++++++++++++')
+const numlist1 =[1,2,3,4]
+const numlist2 =[5,6,7,8]
+numlist1.push(numlist2) // manipulate exixting array
+console.log(numlist1) // [ 1, 2, 3, 4, [ 5, 6, 7, 8 ] ]
 
+numlist1.pop() 
+console.log(numlist1) // [1,2,3,4]
 
+const concatedNumList12 = numlist1.concat(numlist2) // return new array but only concat one array
+console.log(concatedNumList12) // [ 1, 2, 3, 4, 5, 6, 7, 8 ] 
+
+const mergedUsingSpreadList = [...numlist1, ...numlist2] // we can add multiple array
+console.log(concatedNumList12) // [ 1, 2, 3, 4, 5, 6, 7, 8 ] 
+
+const indepthArrayList = [1,[2,[3,4,[5,6,7,[8,9]]]]];
+const convertedInSingleLineArray = indepthArrayList.flat(Infinity);
+console.log(convertedInSingleLineArray) // [ 1, 2, 3, 4, 5, 6, 7, 8 , 9] => flat function use to convert multi level array to single line array
+
+console.log(Array.isArray('anmol')) // false
+console.log(Array.from('anmol')) // [ 'a', 'n', 'm', 'o', 'l' ]
+console.log(Array.from({name:'anmol'})) // [] return empty array because it can not understand to use key or value of an object So it returns empty array
+console.log(Array.of(200,300,400)) // [ 200, 300, 400 ]
